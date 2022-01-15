ed2090966434275daeaad36addddc15e4b90bf35
rdbcasillas <rdbcasillas11@gmail.com>
2022-01-15 12:31:27 +0530

    test files

---
+++ src/components/Explorable.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <p>This is a paragraph</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
